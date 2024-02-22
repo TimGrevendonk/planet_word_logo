90296ecbd555fcd39082c3b23e21ee74abb9f29d
TimGrevendonk <r0831309@student.thomasmore.be>
2024-02-22 14:27:11 +0100

    finsihed delay in animation and plannet svg

---
+++ src/js/planetScript.js
@@ -0,0 +1,53 @@
+
+  
+const body = document.querySelector('body');
+const animations = document.querySelectorAll('[data-animation');
+
+const canvasSize = body.offsetHeight * body.offsetWidth;
+const starsFraction = Math.ceil(canvasSize / 2000);
+
+(function addStars() { 
+    for(let i = 0; i < starsFraction; i++) {
+    // Set coördinates and form of star.
+    let xCoördinate = random(0, 100);
+    let yCoördinate = random(0, 100);
+    let opacity = random(0.5, 1);
+    let size = random(1, 2);
+    let colour = '#F7FDFF';
+        
+    // Add star to body.
+    const star = document.createElement('div');
+    star.style.position = 'absolute';
+    star.style.left = xCoördinate + '%';
+    star.style.top = yCoördinate + '%';
+    star.style.opacity = opacity;
+    star.style.width = size + 'px';
+    star.style.height = size + 'px';
+    star.style.backgroundColor = colour;
+
+    // Prepend so stars are behind other elements.
+    body.prepend(star);
+    }
+}());
+
+function random(min, max) {
+    return min + Math.random() * (max + 1 - min);
+}
+
+
+function toggleAnimations(animations) {
+    animations.forEach(animation => {
+        const running = animation.style.animationPlayState || 'running';
+        animation.style.animationPlayState = running === 'running' ? 'paused' : 'running';
+    })
+}
+
+
+setInterval(
+    function(){
+        toggleAnimations(animations)
+        setTimeout(() => {
+            toggleAnimations(animations)
+        }, 2000);
+    },14000
+);
